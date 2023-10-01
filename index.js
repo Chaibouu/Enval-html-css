abd92346945e00348a041fa0e4bc9f851efaf3f5
Chaibouu <chaibouabdoulwahab@gmail.com>
2023-10-01 02:30:46 +0100

    add enval

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
 let email = document.querySelector('#email');
 let password = document.querySelector('#password');
 let connexion = document.querySelector('#connexion');
+let notification = document.querySelector('.notification');
 
 if (!localStorage.getItem('key')) {
     localStorage.setItem('key',JSON.stringify([]))
 }
 let users = JSON.parse(localStorage.getItem('key'));
-
-let user = {
-    username : '',
-    password : '',
-}
+let a = 0
 
 
 // button ce connecter 
 connexion.addEventListener('click',()=>{
    if (email.value !=='') {
         if (password.value !=='') {
+            let verifUser = users.find((el)=> el.username === email.value);
+            let verifPassword = users.find((el)=> el.password === password.value);
+            if(verifUser && verifPassword){
+                connexion.parentElement.href = "dashboard.html"
+            }
+            
+            if(!verifUser){
+                a++;
+                notification.style.display = 'block';
+                notification.lastElementChild.textContent = 'Veuillez entrer un User Name correct';
+                setTimeout(()=>{
+                    notification.style.display = 'none';
+                },3000)
+            }
+            else if(!verifPassword){
+                a++;
+                notification.style.display = 'block';
+                notification.lastElementChild.textContent = 'Veuillez entrer un mot de passe correct';
+                setTimeout(()=>{
+                    notification.style.display = 'none';
+                },3000)
+            }
+            if(a>=3){
+                let answer = prompt('La question secret quel est votre jeux préféré : ')
+                console.log(answer);
+                email.disabled=true;
+                password.disabled=true;
+                a = 0
+            }
+           
+console.log(a);
             
         }
         else{
@@ -27,4 +55,3 @@ connexion.addEventListener('click',()=>{
     alert('veuillez entrer un utilisateur')
    }
 })
-
