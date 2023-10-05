4c77c460289ac9595ad7820026cad895b32c0257
Chaibouu <chaibouabdoulwahab@gmail.com>
2023-10-05 18:49:22 +0100

    add enval

--- suivi.js
+++ suivi.js
@@ -1,31 +1,35 @@
 let tbodysuivi = document.querySelector('.tbodysuivi');
 let searchsuivi = document.querySelector('#searchsuivi');
 
-
+let titrerapport = ''
 // afficher les donées de suivi
 suivis.forEach(element => {
     tbodysuivi.innerHTML += `<tr>
                               <td class="tr text">${element.lot}</td>
                               <td class="text">${element.etat}</td>
                               <td class="text">${element.date}</td>
-                              <td class="text">${element.rapport}</td>
+                              <td class="text rap">${element.rapport}</td>
                               <td class="TR text">
                                 <a class="voir"><button>voir</button></a>
                               </td>
                             </tr>`
       
 });
-let voir = document.querySelector('.voir');
+let voir = document.querySelectorAll('.voir');
 //button voir page suivi details
-voir.addEventListener('click', () =>{
-  let tiitre ='';
-  suivis.forEach(el, index =>{
-    if(el.rapport === "Disponnible"){
-      tiitre = el.lot;
-      console.log(index);
+voir.forEach(element => {
+  element.addEventListener('click', () =>{
+    let rapp = element.parentElement.parentElement.querySelector('.rap').textContent;
+    if (rapp === "Disponnible") {
+      let tiitre =element.parentElement.parentElement.firstElementChild.textContent;
+      titrerapport = tiitre;
+      localStorage.setItem('cles',JSON.stringify(titrerapport))
     }
-  })
-}) 
+    else{
+      alert('Rapport Non Disponnible')
+    }
+  }) 
+});
  
 
 
