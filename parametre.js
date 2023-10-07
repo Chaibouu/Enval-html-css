9d17622ad394c80e9d98325a0b2cdd53da7e8eb7
Chaibouu <chaibouabdoulwahab@gmail.com>
2023-10-07 05:44:40 +0100

    add enval

--- parametre.js
+++ parametre.js
@@ -1,3 +1,6 @@
 let textarea = document.querySelector('.textarea');
 let avatar = document.querySelector('.avatar');
 let modifphoto = document.querySelector('.modifphoto');
+let supphoto = document.querySelector('.supphoto');
+
+
