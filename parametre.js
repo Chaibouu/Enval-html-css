0d80a1628cc012d10eaa05cd1fbdfce82b884c12
Chaibouu <chaibouabdoulwahab@gmail.com>
2023-10-07 05:56:05 +0100

    add enval

--- parametre.js
+++ parametre.js
@@ -1,6 +1,9 @@
 let textarea = document.querySelector('.textarea');
-let avatar = document.querySelector('.avatar');
+let avatar = document.querySelector('#avatar');
 let modifphoto = document.querySelector('.modifphoto');
 let supphoto = document.querySelector('.supphoto');
 
 
+modifphoto.addEventListener('click',()=>{
+    avatar.src = URL.createObjectURL(modifphoto.file);
+})
